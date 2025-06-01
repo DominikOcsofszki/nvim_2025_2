05be542706ecb379fc15df5ec44ef776b0aa2b37
Dominik Ocsofszki <dominikocsofszki@gmail.com>
2025-06-01 19:58:55 +0200

    Add Neo-Tree configs, add nesting_rules

--- lua/plugins/new/neo-tree.lua
+++ lua/plugins/new/neo-tree.lua
@@ -1,3 +1,13 @@
+local _nesting_rules = function()
+	return {
+		-- ~/.local/share/nvim/lazy/neo-tree.nvim/doc/neo-tree.txt:1391
+		["docker"] = {
+			pattern = "^dockerfile$",
+			ignore_case = true,
+			files = { ".dockerignore", "docker-compose.*", "dockerfile*" },
+		}
+	}
+end
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	version = "*",
@@ -8,15 +18,59 @@ return {
 	},
 	lazy = false,
 	keys = {
-		{ "<leader>e", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+		{ "<leader>e",  ":Neotree toggle left<CR>",  desc = "NeoTree reveal", silent = false },
+		{ "<c-e>",      ":Neotree toggle right<CR>", desc = "NeoTree reveal", silent = true },
+		{ "<c-x><c-e>", ":Neotree toggle float<CR>", desc = "NeoTree reveal", silent = true },
 	},
 	opts = {
-		filesystem = {
-			window = {
-				mappings = {
-					["<leader>e>"] = "close_window",
+		nesting_rules = _nesting_rules(),
+		close_if_last_window = true,
+		use_popups_for_input = false,
+		window = {
+			mappings = {
+				["P"] = {
+					"toggle_preview",
+					config = {
+						use_float = false,
+						use_image_nvim = true,
+					}
 				},
+				["w"] = "open_with_window_picker",
+				-- ["C"] = "close_node",
+				-- ["C"] = "close_all_subnodes",
+				["z"] = "close_all_subnodes",
+				-- ["z"] = "close_all_nodes",
+				["Z"] = "expand_all_subnodes",
 			},
 		},
-	},
+
+		filesystem = {
+			filtered_items = {
+				hide_by_name = {
+					".DS_Store",
+					"thumbs.db",
+					"node_modules",
+					"tmp.lua",
+					"tmp.py",
+				},
+				-- hide_by_pattern = { -- uses glob style patterns
+				-- 	--"*.meta",
+				-- 	--"*/src/*/tsconfig.json"
+				-- },
+				-- always_show = { -- remains visible even if other settings would normally hide it
+				-- 	--".gitignored",
+				-- },
+				-- always_show_by_pattern = { -- uses glob style patterns
+				-- 	--".env*",
+				-- },
+				-- never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+				-- 	--".DS_Store",
+				-- 	--"thumbs.db"
+				-- },
+				-- never_show_by_pattern = { -- uses glob style patterns
+				-- 	--".null-ls_*",
+				-- },
+			},
+		},
+	}
 }
