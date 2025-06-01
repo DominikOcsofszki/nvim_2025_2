68d64bb8f9cf84b50ff6f0a05323257544a8fd9c
Dominik Ocsofszki <dominikocsofszki@gmail.com>
2025-06-01 15:38:24 +0200

    Add folding to options

--- lua/config/optional/settings.lua
+++ lua/config/optional/settings.lua
@@ -13,3 +13,19 @@ vim.opt.inccommand = "split"
 vim.opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
 vim.opt.mouse = "a"
 vim.opt.undofile = true
+
+-- ==================================
+-- =============FOLDING==============
+vim.o.fillchars = 'eob: ,fold: ,foldopen:,foldsep: ,foldclose:'
+vim.o.foldcolumn = '1'
+vim.o.foldenable = true
+vim.o.foldexpr = 'v:lua.vim.lsp.foldexpr()'
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldmethod = 'expr'
+-- vim.o.foldminlines  = 4
+-- vim.o.foldnestmax = 3
+-- -- vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+-- =============FOLDING==============
+-- ==================================
+
