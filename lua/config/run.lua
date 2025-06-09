72babad9e7c886efd62bbf57ec69cc12fba6ed33
Dominik Ocsofszki <dominikocsofszki@gmail.com>
2025-06-09 16:57:16 +0200

    feat: Add run command and ignore tmp directory
    Includes a run command that executes files based on filetype.
    Also adds tmp/** to .gitignore, with exception for plugins.
    adds lua/config/run.lua and keymap to run
    on `<leader>r`.

---
+++ lua/config/run.lua
@@ -0,0 +1,27 @@
+local command_templates = {
+	sh = 'bash %',
+	lua = 'luafile %',
+	python = 'python %',
+	py = 'python %',
+	javascript = 'node %',
+	typescript = 'ts-node %',
+	ruby = 'ruby %',
+	perl = 'perl %',
+	php = 'php %',
+	go = 'go run %',
+	c = 'gcc % -o out && ./out',
+	cpp = 'g++ % -o out && ./out',
+}
+
+local run = function()
+	local ft = vim.bo.filetype
+	local cmd = command_templates[ft]
+	if cmd then
+		vim.cmd(cmd)
+	else
+		vim.print("command for: " .. ft .. " does not exist")
+	end
+end
+
+
+vim.keymap.set('n', '<leader>r', run, {})
