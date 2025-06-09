2fa02c8abfce672b880f6f53ba00cbb577057324
Dominik Ocsofszki <dominikocsofszki@gmail.com>
2025-06-09 17:58:59 +0200

    fix: Ignore tmp/plugins and add shebangs Add shebangs for commands in run.lua and ignore tmp/plugins
    This improves execution and reduces noise.
    Improves execution of various languages by prepending
    "!" for shell commands.

--- lua/config/run.lua
+++ lua/config/run.lua
@@ -1,22 +1,25 @@
 local command_templates = {
-	sh = 'bash %',
+	sh = '!sh %',
+	zsh = '!sh %',
 	lua = 'luafile %',
-	python = 'python %',
-	py = 'python %',
-	javascript = 'node %',
-	typescript = 'ts-node %',
-	ruby = 'ruby %',
-	perl = 'perl %',
-	php = 'php %',
-	go = 'go run %',
-	c = 'gcc % -o out && ./out',
-	cpp = 'g++ % -o out && ./out',
+	python = '!python %',
+	py = '!python %',
+	javascript = '!node %',
+	typescript = '!ts-node %',
+	ruby = '!ruby %',
+	perl = '!perl %',
+	php = '!php %',
+	go = '!go run %',
+	c = '!gcc % -o out && ./out',
+	cpp = '!g++ % -o out && ./out',
 }
 
 local run = function()
+	vim.cmd("w")
 	local ft = vim.bo.filetype
 	local cmd = command_templates[ft]
 	if cmd then
+		vim.print(cmd)
 		vim.cmd(cmd)
 	else
 		vim.print("command for: " .. ft .. " does not exist")
